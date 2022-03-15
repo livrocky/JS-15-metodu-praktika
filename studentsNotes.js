6baec03e6722e57849285365aee4e57a3294525b
rokas <rokasra@gmail.com>
2022-03-15 19:32:58 +0200

    added student notes

--- studentsNotes.js
+++ studentsNotes.js
@@ -50,60 +50,33 @@ const students = [
   },
 ];
 
-// st1. sugeneruoti studentu nerikiuota sarasa htmle kuriame butu studentu vardai
-// function genList() {
-//   const ulEl = document.createElement("ul");
-//   ulEl.innerHTML = students.map((studObj) => `<li> ${studObj.name} <button class="del-stud">Delete</button> </li>`).join("");
-
-//_______________________-//
-// ulEl.innerHTML = students.reduce((totalString, studObj) => totalString + `<li> ${studObj.name} </li>`, '');
-//_________________________//
-
-//   document.body.append(ulEl);
-// }
-// genList();
-
 // st2. Parasyti funkcija, kuriai paduodam miesta kaip argumenta ir ji atrenka studentus is to miesto
 const kaunieciai = studentsFrom("Kaunas");
-// console.log("kaunieciai ===", kaunieciai);
 function studentsFrom(miestas) {
   return students.filter((studObj) => studObj.town === miestas);
 }
 
-// st3. Parasyti funkcija, kuriai paduodam amziu kaip argumenta ir ji atrenka studentus kuriu amzius didesnis uz argumenta.
-
 // st4. prie st1 sugeneruoto saraso pridedam mygtuka su textu 'delete'. mygtuka paspaudus istiname ta li el kuriame yra paspaustas mygtukas
 document.body.addEventListener("click", (e) => {
-  // console.log('clicked on', e.target);
-  // suzinoti ar paspaude ant el kuris turi klase 'del-stud'
   if (e.target.classList.contains("del-stud")) {
-    // console.log('delete', e.target.parentElement);
-    // e.target yra button, o istrinti norim li el
     e.target.parentElement.remove();
   }
 });
 // st5. is studentu masyvo infomracijos sugeneruoti korteles htmle.
 // korteles turi tureti apvada ir visa su studentu susijusia info. korteles turi tilpti 3 per puslapio ploti.
-{
-  /* <div class="stud-card">
-  <h3>James</h3>
-  <p>A man from Vilnius</p>
-  <p>He is 25 years old and has a car</p>
-</div> */
-}
+
 function makeStudCard(stObj) {
   const gender = stObj.gender === "male" ? "man" : "woman";
   const who = stObj.gender === "male" ? "He" : "She";
   const car = stObj.hasCar ? "and has a car" : "";
 
   return `
-  <div class="stud-card">
-    <h3>${stObj.name}</h3>
-    <p>A ${gender} from ${stObj.town}</p>
-    <p>${who} is ${stObj.age} years old ${car}</p>
-  </div>
-  `;
-  // console.log('studString ===', studString);
+    <div class="stud-card">
+      <h3>${stObj.name}</h3>
+      <p>A ${gender} from ${stObj.town}</p>
+      <p>${who} is ${stObj.age} years old ${car}</p>
+    </div>
+    `;
 }
 const divEl = document.createElement("div");
 divEl.className = "grid";
@@ -126,5 +99,3 @@ const vilniusBtn = document.getElementById("vilnius");
 allBtn.addEventListener("click", () => generateCards(students));
 kaunasBtn.addEventListener("click", () => generateCards(studentsFrom("Kaunas")));
 vilniusBtn.addEventListener("click", () => generateCards(students.filter((studObj) => studObj.town === "Vilnius")));
-
-// st6 paspaudus atitinkamus mygtukus mes htmle atvaizduojam visus, kauniecius arba vilniecius korteliu pavidalu (st5)
